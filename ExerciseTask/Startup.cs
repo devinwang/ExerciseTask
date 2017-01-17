a793f0e915f41785ab4021d811e0f495e49f7bf5
Devin <Devin@DESKTOP-BE0U4RJ>
2017-01-17 15:22:03 +1300

    Add project files.

---
+++ ExerciseTask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExerciseTask.Startup))]
+namespace ExerciseTask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
